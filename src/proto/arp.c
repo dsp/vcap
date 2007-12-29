bdd31ec9c5edf1e7d11cd402d1d670526a8da3ad
David Soria Parra <dsp@php.net>
2007-12-29 20:29:53 +0100

    Mark parameter as unused
    Signed-off-by: David Soria Parra <dsp@php.net>

--- src/proto/arp.c
+++ src/proto/arp.c
@@ -20,7 +20,7 @@
 static struct vcap_data_entry *arp_node;
 
 void
-vcap_packet_arp (const u_char * packet)
+vcap_packet_arp (const u_char * packet __attribute__((unused)))
 {
   VCAP_DATA_CREATE (arp_node, "arp", "eth") VCAP_DATA_INC (arp_node);
 }
